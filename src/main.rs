13f1ab0e5f48e3e4c37e491fd94218a6d3a1aab7
jrheard <jrheard@cs.stanford.edu>
2019-02-04 10:28:46 -0800

    2a

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs;
 
@@ -23,6 +24,8 @@ fn one_b() -> i32 {
 
     let mut seen_frequencies: HashSet<i32> = HashSet::new();
     for frequency in reductions {
+        // TODO ask peter why i have to do &frequency here.
+        // Aren't i32s Copy? Shouldn't that mean that they're fine to pass around?
         if seen_frequencies.contains(&frequency) {
             return frequency;
         } else {
@@ -33,7 +36,39 @@ fn one_b() -> i32 {
     -1
 }
 
+fn two_a() -> i32 {
+    let contents = fs::read_to_string("src/inputs/2.txt").unwrap();
+
+    let mut num_with_a_letter_that_appears_twice = 0;
+    let mut num_with_a_letter_that_appears_thrice = 0;
+
+    for letter_freq_map in contents.lines().map(letter_frequencies) {
+        if letter_freq_map.values().into_iter().any(|&x| x == 2) {
+            num_with_a_letter_that_appears_twice += 1;
+        }
+        if letter_freq_map.values().into_iter().any(|&x| x == 3) {
+            num_with_a_letter_that_appears_thrice += 1;
+        }
+    }
+
+    num_with_a_letter_that_appears_twice * num_with_a_letter_that_appears_thrice
+}
+
+fn letter_frequencies(x: &str) -> HashMap<String, i32> {
+    let mut ret = HashMap::new();
+
+    for character in x.chars() {
+        let count = ret.entry(character.to_string()).or_insert(0);
+        *count += 1;
+    }
+
+    ret
+}
+
 fn main() {
     println!("1a: {}", one_a());
     println!("1b: {}", one_b());
+    println!("2a: {}", two_a());
+
+
 }
