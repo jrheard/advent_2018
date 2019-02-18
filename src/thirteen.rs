98ce309eb9121abdd400c870414bba0206a5946e
jrheard <jrheard@cs.stanford.edu>
2019-02-18 13:32:01 -0800

    tidy 13

--- src/thirteen.rs
+++ src/thirteen.rs
@@ -99,6 +99,8 @@ fn parse_input(filename: &str) -> Mine {
         grid.push(row);
     }
 
+    // grid is indexed by [y][x], but I prefer indexing by [x][y], so let's rotate it.
+
     let mut ret_grid = vec![vec![MineSpace::Empty; grid.len()]; grid[0].len()];
 
     for x in 0..ret_grid.len() {
@@ -123,25 +125,33 @@ impl Mine {
         parse_input(filename)
     }
 
-    /// Advances time one tick. Returns Ok(()) if there were no crashes, Err((x, y)) if there was a crash.
+    /// Advances time one tick. Returns a vector containing the locations of any crashes that occurred.
+    /// Removes any carts that were affected by a crash this tick from self.carts.
     fn tick(&mut self) -> Vec<(usize, usize)> {
+        // "Carts all move at the same speed; they take turns moving a single step at a time.
+        // They do this based on their current location: carts on the top row move first (acting
+        // from left to right), then carts on the second row move (again from left to right),
+        // then carts on the third row, and so on."
         self.carts.sort_by_key(|cart| (cart.y, cart.x));
 
         let mut cart_positions = self
             .carts
             .iter()
-            .map(|cart| (cart.x, cart.y, cart.direction))
-            .collect::<Vec<(usize, usize, Direction)>>();
+            .map(|cart| (cart.x, cart.y))
+            .collect::<Vec<(usize, usize)>>();
 
         let mut crash_sites = vec![];
 
         for cart in &mut self.carts {
-            cart_positions.retain(|&position| (cart.x, cart.y, cart.direction) != position);
+            // Remove our old position from `cart_positions`.
+            cart_positions.retain(|&position| (cart.x, cart.y) != position);
 
+            // There's a crash here, this cart is destroyed and will be removed after this loop!
             if crash_sites.contains(&(cart.x, cart.y)) {
                 continue;
             }
 
+            // Move the cart.
             match cart.direction {
                 North => cart.y -= 1,
                 East => cart.x += 1,
@@ -150,14 +160,14 @@ impl Mine {
             }
 
             // Check for crashes!
-            for (x, y, direction) in &cart_positions {
-                if *x == cart.x && *y == cart.y && *direction != cart.direction {
+            for (x, y) in &cart_positions {
+                if *x == cart.x && *y == cart.y {
                     crash_sites.push((*x, *y));
                 }
             }
 
-            let space = self.grid[cart.x][cart.y];
-            match space {
+            // Adjust the cart's direction based on the piece of track it's now on.
+            match self.grid[cart.x][cart.y] {
                 MineSpace::CurveRight => {
                     cart.direction = match cart.direction {
                         North => East,
@@ -175,6 +185,10 @@ impl Mine {
                     }
                 }
                 MineSpace::Intersection => {
+                    // "Each time a cart has the option to turn (by arriving at any intersection),
+                    // it turns left the first time, goes straight the second time, turns right the third time,
+                    // and then repeats those directions starting again with left the fourth time,
+                    // straight the fifth time, and so on."
                     if cart.turn_counter == 0 {
                         cart.direction = cart.direction.left();
                     } else if cart.turn_counter == 2 {
@@ -188,9 +202,10 @@ impl Mine {
                 _ => (),
             }
 
-            cart_positions.push((cart.x, cart.y, cart.direction));
+            cart_positions.push((cart.x, cart.y));
         }
 
+        // Remove any carts that were involved in a crash this tick.
         self.carts
             .retain(|cart| !crash_sites.contains(&(cart.x, cart.y)));
 
@@ -198,17 +213,8 @@ impl Mine {
     }
 }
 
-pub fn thirteen_b() -> (usize, usize) {
-    let mut mine = Mine::new("src/inputs/13.txt");
-
-    loop {
-        let _ = mine.tick();
-        if mine.carts.len() == 1 {
-            return (mine.carts[0].x, mine.carts[0].y);
-        }
-    }
-}
-
+/// After following their respective paths for a while, the carts eventually crash.
+/// To help prevent crashes, you'd like to know the location of the first crash.
 pub fn thirteen_a() -> (usize, usize) {
     let mut mine = Mine::new("src/inputs/13.txt");
 
@@ -216,6 +222,23 @@ pub fn thirteen_a() -> (usize, usize) {
         let crashes = mine.tick();
         if !crashes.is_empty() {
             return crashes[0];
+        }
+    }
+}
+
+/// There isn't much you can do to prevent crashes in this ridiculous system.
+/// However, by predicting the crashes, the Elves know where to be in advance and
+/// instantly remove the two crashing carts the moment any crash occurs.
+/// They can proceed like this for a while, but eventually, they're going to run out of carts.
+/// It could be useful to figure out where the last cart that hasn't crashed will end up.
+/// What is the location of the last cart at the end of the first tick where it is the only cart left?
+pub fn thirteen_b() -> (usize, usize) {
+    let mut mine = Mine::new("src/inputs/13.txt");
+
+    loop {
+        let _ = mine.tick();
+        if mine.carts.len() == 1 {
+            return (mine.carts[0].x, mine.carts[0].y);
         }
     }
 }
