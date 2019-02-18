e02b743f847a21f4ba0095b4eb487c962011f0ef
jrheard <jrheard@cs.stanford.edu>
2019-02-18 12:18:46 -0800

    WIP on 13a

--- src/thirteen.rs
+++ src/thirteen.rs
@@ -1,6 +1,8 @@
 use std::fs;
 
-#[derive(Clone, Copy)]
+use itertools::Itertools;
+
+#[derive(Clone, Copy, Debug)]
 enum MineSpace {
     StraightVertical,   // |
     StraightHorizontal, // -
@@ -10,6 +12,7 @@ enum MineSpace {
     Empty,
 }
 
+#[derive(Debug, PartialEq)]
 enum Direction {
     North,
     East,
@@ -17,16 +20,49 @@ enum Direction {
     West,
 }
 
+use Direction::*;
+
+impl Direction {
+    fn right(&self) -> Direction {
+        match self {
+            North => East,
+            East => South,
+            South => West,
+            West => North,
+        }
+    }
+
+    fn left(&self) -> Direction {
+        match self {
+            North => West,
+            East => North,
+            South => East,
+            West => South,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct MineCart {
     x: usize,
     y: usize,
+    turn_counter: u32,
     direction: Direction,
 }
 
-type Grid = Vec<Vec<MineSpace>>;
+impl MineCart {
+    fn new(x: usize, y: usize, direction: Direction) -> Self {
+        MineCart {
+            x,
+            y,
+            direction,
+            turn_counter: 0,
+        }
+    }
+}
 
-fn parse_input() -> (Grid, Vec<MineCart>) {
-    let contents = fs::read_to_string("src/inputs/13_sample.txt").unwrap();
+fn parse_input(filename: &str) -> Mine {
+    let contents = fs::read_to_string(filename).unwrap();
 
     let mut grid = vec![];
     let mut carts = vec![];
@@ -37,35 +73,19 @@ fn parse_input() -> (Grid, Vec<MineCart>) {
             let value = match character {
                 ' ' => MineSpace::Empty,
                 '>' => {
-                    carts.push(MineCart {
-                        x,
-                        y,
-                        direction: Direction::East,
-                    });
+                    carts.push(MineCart::new(x, y, East));
                     MineSpace::StraightHorizontal
                 }
                 '<' => {
-                    carts.push(MineCart {
-                        x,
-                        y,
-                        direction: Direction::West,
-                    });
+                    carts.push(MineCart::new(x, y, West));
                     MineSpace::StraightHorizontal
                 }
                 '^' => {
-                    carts.push(MineCart {
-                        x,
-                        y,
-                        direction: Direction::North,
-                    });
+                    carts.push(MineCart::new(x, y, North));
                     MineSpace::StraightVertical
                 }
                 'v' => {
-                    carts.push(MineCart {
-                        x,
-                        y,
-                        direction: Direction::South,
-                    });
+                    carts.push(MineCart::new(x, y, South));
                     MineSpace::StraightVertical
                 }
                 '|' => MineSpace::StraightVertical,
@@ -89,10 +109,64 @@ fn parse_input() -> (Grid, Vec<MineCart>) {
         }
     }
 
-    (ret_grid, carts)
+    Mine {
+        carts,
+        grid: ret_grid,
+    }
+}
+
+struct Mine {
+    carts: Vec<MineCart>,
+    grid: Vec<Vec<MineSpace>>,
+}
+
+impl Mine {
+    fn new(filename: &str) -> Mine {
+        parse_input(filename)
+    }
+
+    // xxx return value
+    fn tick(&mut self) {
+        let carts = self.carts.iter().sorted_by_key(|cart| (cart.y, cart.x));
+
+        for cart in &mut self.carts {
+            match cart.direction {
+                North => cart.y -= 1,
+                East => cart.x += 1,
+                South => cart.y += 1,
+                West => cart.x -= 1,
+            }
+
+            // TODO collision checking
+
+            let space = self.grid[cart.x][cart.y];
+            match space {
+                MineSpace::CurveLeft => {
+                    cart.direction = if cart.direction == East { South } else { West };
+                }
+                MineSpace::CurveRight => {
+                    cart.direction = if cart.direction == West { South } else { East };
+                }
+                MineSpace::Intersection => {
+                    if cart.turn_counter == 0 {
+                        cart.direction = cart.direction.left();
+                    } else if cart.turn_counter == 2 {
+                        cart.direction = cart.direction.right();
+                    }
+
+                    cart.turn_counter += 1;
+                    cart.turn_counter %= 3;
+                }
+                MineSpace::Empty => panic!("a cart fell off the map: {:?}", cart),
+                _ => (),
+            }
+        }
+    }
 }
 
 pub fn thirteen_a() -> i32 {
+    let mine = Mine::new("src/inputs/13_sample.txt");
+
     5
 }
 
