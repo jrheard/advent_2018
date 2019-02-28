5c7473925ef0213c0b4272dc9ab144dff5dc5f54
jrheard <jrheard@cs.stanford.edu>
2019-02-27 16:56:27 -0800

    fun with criterion + aborted optimization

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,12 +1,18 @@
+use std::time;
+
 #[macro_use]
 extern crate criterion;
-use criterion::Criterion;
+use criterion::{Benchmark, Criterion};
 
 extern crate advent_2018;
 use advent_2018::fifteen;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("15b", |b| b.iter(|| fifteen::fifteen_b("src/inputs/15.txt")));
+    let benchmark = Benchmark::new("15b", |b| b.iter(|| fifteen::fifteen_b("src/inputs/15.txt")))
+        .sample_size(20)
+        .measurement_time(time::Duration::new(180, 0));
+
+    c.bench("15b", benchmark);
 }
 
 criterion_group!(benches, criterion_benchmark);
