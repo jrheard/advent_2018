0ef08c1545b98de5c2f02ecd6b40be42172d96ae
jrheard <jrheard@cs.stanford.edu>
2019-03-02 17:05:23 -0800

    implement 16b

--- src/sixteen.rs
+++ src/sixteen.rs
@@ -11,20 +11,20 @@ use serde_scan::scan;
 
 #[derive(Debug, PartialEq)]
 struct Sample {
-    before: [u8; 4],
+    before: [usize; 4],
     instruction: [usize; 4],
-    after: [u8; 4],
+    after: [usize; 4],
 }
 
-fn parse_input() -> Vec<Sample> {
+fn parse_input() -> (Vec<Sample>, Vec<[usize; 4]>) {
     let contents = fs::read_to_string("src/inputs/16.txt").unwrap();
 
-    let mut ret = vec![];
+    let mut first_half = vec![];
 
     for mut chunk in &contents.lines().chunks(4) {
         let before = chunk.next().unwrap();
         if !before.starts_with("Before") {
-            // "The manual also includes a small test program (the second section of your puzzle input) - you can ignore it for now.""
+            // We've reached the second part of the puzzle input.
             break;
         }
 
@@ -34,50 +34,73 @@ fn parse_input() -> Vec<Sample> {
         let after = chunk.next().unwrap();
         let (a_a, a_b, a_c, a_d) = scan!("After: [{}, {}, {}, {}]" <- after).unwrap();
 
-        ret.push(Sample {
+        first_half.push(Sample {
             before: [b_a, b_b, b_c, b_d],
             instruction: [i_a, i_b, i_c, i_d],
             after: [a_a, a_b, a_c, a_d],
         });
     }
 
-    ret
+    let mut second_half_index = 0;
+    let mut newlines_seen_in_a_row = 0;
+    let lines = contents.lines().collect::<Vec<&str>>();
+
+    for (i, &line) in lines.iter().enumerate() {
+        if line == "" {
+            newlines_seen_in_a_row += 1;
+        } else {
+            newlines_seen_in_a_row = 0;
+        }
+
+        if newlines_seen_in_a_row == 3 {
+            second_half_index = i + 1;
+            break;
+        }
+    }
+
+    let mut second_half = vec![];
+
+    for &line in &lines[second_half_index..] {
+        second_half.push(scan!("{} {} {} {}" <- line).unwrap());
+    }
+
+    (first_half, second_half)
 }
 
-type Operation = Fn(&mut [u8; 4], usize, usize, usize);
+type Operation = Fn(&mut [usize; 4], usize, usize, usize);
 
 fn get_operations() -> Vec<Box<Operation>> {
     vec![
         // "addr (add register) stores into register C the result of adding register A and register B."
         Box::new(|registers, a, b, c| registers[c] = registers[a] + registers[b]),
         // "addi (add immediate) stores into register C the result of adding register A and value B."
-        Box::new(|registers, a, b, c| registers[c] = registers[a] + b as u8),
+        Box::new(|registers, a, b, c| registers[c] = registers[a] + b),
         // "mulr (multiply register) stores into register C the result of multiplying register A and register B."
         Box::new(|registers, a, b, c| registers[c] = registers[a] * registers[b]),
         // "muli (multiply immediate) stores into register C the result of multiplying register A and value B."
-        Box::new(|registers, a, b, c| registers[c] = registers[a] * b as u8),
+        Box::new(|registers, a, b, c| registers[c] = registers[a] * b),
         // "banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B."
         Box::new(|registers, a, b, c| registers[c] = registers[a] & registers[b]),
         // "bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B."
-        Box::new(|registers, a, b, c| registers[c] = registers[a] & b as u8),
+        Box::new(|registers, a, b, c| registers[c] = registers[a] & b),
         // "borr (bitwise OR register) stores into register C the result of the bitwise OR of register A and register B."
         Box::new(|registers, a, b, c| registers[c] = registers[a] | registers[b]),
         // "bori (bitwise OR immediate) stores into register C the result of the bitwise OR of register A and value B."
-        Box::new(|registers, a, b, c| registers[c] = registers[a] | b as u8),
+        Box::new(|registers, a, b, c| registers[c] = registers[a] | b),
         // "setr (set register) copies the contents of register A into register C. (Input B is ignored.)"
         Box::new(|registers, a, _, c| registers[c] = registers[a]),
         // "seti (set immediate) stores value A into register C. (Input B is ignored.)"
-        Box::new(|registers, a, _, c| registers[c] = a as u8),
+        Box::new(|registers, a, _, c| registers[c] = a),
         // "gtir (greater-than immediate/register) sets register C to 1 if value A is greater than register B. Otherwise, register C is set to 0."
-        Box::new(|registers, a, b, c| registers[c] = if a as u8 > registers[b] { 1 } else { 0 }),
+        Box::new(|registers, a, b, c| registers[c] = if a > registers[b] { 1 } else { 0 }),
         // "gtri (greater-than register/immediate) sets register C to 1 if register A is greater than value B. Otherwise, register C is set to 0."
-        Box::new(|registers, a, b, c| registers[c] = if registers[a] > b as u8 { 1 } else { 0 }),
+        Box::new(|registers, a, b, c| registers[c] = if registers[a] > b { 1 } else { 0 }),
         // "gtrr (greater-than register/register) sets register C to 1 if register A is greater than register B. Otherwise, register C is set to 0."
         Box::new(|registers, a, b, c| registers[c] = if registers[a] > registers[b] { 1 } else { 0 }),
         // "eqir (equal immediate/register) sets register C to 1 if value A is equal to register B. Otherwise, register C is set to 0."
-        Box::new(|registers, a, b, c| registers[c] = if a as u8 == registers[b] { 1 } else { 0 }),
+        Box::new(|registers, a, b, c| registers[c] = if a == registers[b] { 1 } else { 0 }),
         // "eqri (equal register/immediate) sets register C to 1 if register A is equal to value B. Otherwise, register C is set to 0."
-        Box::new(|registers, a, b, c| registers[c] = if registers[a] == b as u8 { 1 } else { 0 }),
+        Box::new(|registers, a, b, c| registers[c] = if registers[a] == b { 1 } else { 0 }),
         // "eqrr (equal register/register) sets register C to 1 if register A is equal to register B. Otherwise, register C is set to 0."
         Box::new(|registers, a, b, c| registers[c] = if registers[a] == registers[b] { 1 } else { 0 }),
     ]
@@ -104,7 +127,7 @@ fn test_sample(sample: &Sample) -> Vec<usize> {
 
 /// Ignoring the opcode numbers, how many samples in your puzzle input behave like three or more opcodes?
 pub fn sixteen_a() -> usize {
-    let samples = parse_input();
+    let (samples, _) = parse_input();
 
     samples
         .iter()
@@ -144,7 +167,7 @@ fn find_mapping(mut possibilities: HashMap<usize, HashSet<usize>>) -> HashMap<us
     mapping
 }
 
-fn compute_opcode_to_operation_mapping(samples: &[Sample]) -> [u8; 16] {
+fn compute_opcode_to_operation_mapping(samples: &[Sample]) -> [usize; 16] {
     // `possibilities` is a map of opcode -> possible operation index.
     // It'll have entries like {5: #{2, 4, 11}}.
     let mut possibilities = HashMap::new();
@@ -173,7 +196,7 @@ fn compute_opcode_to_operation_mapping(samples: &[Sample]) -> [u8; 16] {
     let mut ret = [0; 16];
 
     for i in 0..16 {
-        ret[i] = mapping[&i] as u8;
+        ret[i] = mapping[&i];
     }
 
     ret
@@ -181,15 +204,19 @@ fn compute_opcode_to_operation_mapping(samples: &[Sample]) -> [u8; 16] {
 
 /// Using the samples you collected, work out the number of each opcode and execute the test program
 /// (the second section of your puzzle input). What value is contained in register 0 after executing the test program?
-pub fn sixteen_b() -> u8 {
-    let samples = parse_input();
-
+pub fn sixteen_b() -> usize {
+    let (samples, program) = parse_input();
     let opcodes_to_operation_indexes = compute_opcode_to_operation_mapping(&samples);
-    dbg!(opcodes_to_operation_indexes);
+    let operations = get_operations();
+    let mut registers = [0, 0, 0, 0];
 
-    // 2. update parse fn to handle second part of file
-    // 3. run program
-    5
+    for line in program {
+        let (a, b, c, d) = (line[0], line[1], line[2], line[3]);
+        let operation = &operations[opcodes_to_operation_indexes[a]];
+        operation(&mut registers, b, c, d);
+    }
+
+    registers[0]
 }
 
 #[cfg(test)]
@@ -199,11 +226,12 @@ mod test {
     #[test]
     fn test_solution() {
         assert_eq!(sixteen_a(), 588);
+        assert_eq!(sixteen_b(), 627);
     }
 
     #[test]
     fn test_parse_input() {
-        let samples = parse_input();
+        let (samples, _) = parse_input();
 
         assert_eq!(samples.len(), 776);
 
